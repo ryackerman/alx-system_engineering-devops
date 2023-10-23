0c0917bfe48e53045bec316485b3035cb4781ea1
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-10-23 16:50:15 -0700

    Resolved conflicts

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Changing the config file with Puppet.
+
+include stdlib
+
+file_line { 'Turn off passwd auth' :
+	ensure  => present,
+	path    => '/etc/ssh/ssh_config',
+	line    => 'PasswordAuthentication no',
+	replace => true,
+}
+
+file_line { 'Declare identity file' :
+	ensure  => present,
+	path    => '/etc/ssh/ssh_config',
+	line    => 'IdentityFile ~/.ssh/school',
+	replace => true,
+}
