555219f791481d1a1d178184603723be2fe18538
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-11-07 07:34:25 -0800

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number of subscribers 
+(not active users, total subscribers) for a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent":"KLYD"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
