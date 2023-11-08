ee4066aee98be45fbbf5cbe4396c1f760942f2bb
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-11-07 23:29:00 -0800

    fixing code style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent":"KLYD"
+        "User-Agent": "KLYD"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
