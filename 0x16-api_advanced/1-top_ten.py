91f10ffe1ed3b3e5e3301193ea3d2c7b40fe451b
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-11-07 23:31:52 -0800

    fixing code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """prints the titles of the first 10 hot posts."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "KLYD"
